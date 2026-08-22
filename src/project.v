677122f1bf5fff59c4a8a81dd723ce9a8bda0da9
mauro <269277793+mauro-ciccone@users.noreply.github.com>
2026-08-22 17:22:19 +0200

    chore: stress test 30 neurons 80 synapses

--- src/project.v
+++ src/project.v
@@ -35,6 +35,16 @@ module tt_um_example (
     reg [9:0] neuron_17;
     reg [9:0] neuron_18;
     reg [9:0] neuron_19;
+    reg [9:0] neuron_20;
+    reg [9:0] neuron_21;
+    reg [9:0] neuron_22;
+    reg [9:0] neuron_23;
+    reg [9:0] neuron_24;
+    reg [9:0] neuron_25;
+    reg [9:0] neuron_26;
+    reg [9:0] neuron_27;
+    reg [9:0] neuron_28;
+    reg [9:0] neuron_29;
 
     always @(posedge clk) begin
         if (!rst_n) begin
@@ -58,27 +68,47 @@ module tt_um_example (
             neuron_17 <= 10'd0;
             neuron_18 <= 10'd0;
             neuron_19 <= 10'd0;
+            neuron_20 <= 10'd0;
+            neuron_21 <= 10'd0;
+            neuron_22 <= 10'd0;
+            neuron_23 <= 10'd0;
+            neuron_24 <= 10'd0;
+            neuron_25 <= 10'd0;
+            neuron_26 <= 10'd0;
+            neuron_27 <= 10'd0;
+            neuron_28 <= 10'd0;
+            neuron_29 <= 10'd0;
         end else begin
-            neuron_0 <= (neuron_0 >> 3) + (neuron_6 >> 2) + (neuron_5 >> 2);
-            neuron_1 <= (neuron_1 >> 3) + (neuron_18 >> 2) + (neuron_11 >> 2);
-            neuron_2 <= (neuron_2 >> 3) + (neuron_13 << 1) + (neuron_6 << 1);
-            neuron_3 <= (neuron_3 >> 3) + (neuron_19 << 1) + (neuron_14 << 1);
-            neuron_4 <= (neuron_4 >> 3) + (neuron_1 >> 2) + (neuron_13 << 1);
-            neuron_5 <= (neuron_5 >> 3) + (neuron_5 >> 2) + (neuron_17 << 1);
-            neuron_6 <= (neuron_6 >> 3) + (neuron_16 >> 2) + (neuron_7 >> 1);
-            neuron_7 <= (neuron_7 >> 3) + (neuron_11 >> 1) + (neuron_12 >> 1);
-            neuron_8 <= (neuron_8 >> 3) + (neuron_2 >> 1) + (neuron_11 >> 1);
-            neuron_9 <= (neuron_9 >> 3) + (neuron_14 >> 2) + (neuron_0 >> 1);
-            neuron_10 <= (neuron_10 >> 3) + (neuron_8 >> 2) + (neuron_19 << 1);
-            neuron_11 <= (neuron_11 >> 3) + (neuron_3 >> 1) + (neuron_12 >> 2);
-            neuron_12 <= (neuron_12 >> 3) + (neuron_0 >> 1) + (neuron_17 << 1);
-            neuron_13 <= (neuron_13 >> 3) + (neuron_15 >> 2) + (neuron_13 >> 2);
-            neuron_14 <= (neuron_14 >> 3) + (neuron_19 >> 1) + (neuron_3 >> 2);
-            neuron_15 <= (neuron_15 >> 3) + (neuron_6 >> 2) + (neuron_11 >> 1);
-            neuron_16 <= (neuron_16 >> 3) + (neuron_7 >> 1) + (neuron_7 >> 2);
-            neuron_17 <= (neuron_17 >> 3) + (neuron_11 << 1) + (neuron_1 << 1);
-            neuron_18 <= (neuron_18 >> 3) + (neuron_7 >> 2) + (neuron_10 >> 1);
-            neuron_19 <= (neuron_19 >> 3) + (neuron_8 >> 2) + (neuron_8 >> 1);
+            neuron_0 <= (neuron_0 >> 3) + (neuron_19 << 1) + (neuron_15 >> 2);
+            neuron_1 <= (neuron_1 >> 3) + (neuron_17 >> 2) + (neuron_26 >> 1);
+            neuron_2 <= (neuron_2 >> 3) + (neuron_1 >> 1) + (neuron_17 >> 1);
+            neuron_3 <= (neuron_3 >> 3) + (neuron_1 << 1) + (neuron_26 >> 2);
+            neuron_4 <= (neuron_4 >> 3) + (neuron_3 << 1) + (neuron_19 << 1);
+            neuron_5 <= (neuron_5 >> 3) + (neuron_27 << 1) + (neuron_2 >> 1);
+            neuron_6 <= (neuron_6 >> 3) + (neuron_5 >> 1) + (neuron_8 << 1);
+            neuron_7 <= (neuron_7 >> 3) + (neuron_19 >> 1) + (neuron_23 >> 2);
+            neuron_8 <= (neuron_8 >> 3) + (neuron_13 >> 1) + (neuron_10 << 1);
+            neuron_9 <= (neuron_9 >> 3) + (neuron_18 >> 2) + (neuron_23 << 1);
+            neuron_10 <= (neuron_10 >> 3) + (neuron_11 >> 2) + (neuron_14 >> 1);
+            neuron_11 <= (neuron_11 >> 3) + (neuron_20 >> 1) + (neuron_21 >> 2);
+            neuron_12 <= (neuron_12 >> 3) + (neuron_28 >> 2) + (neuron_18 << 1);
+            neuron_13 <= (neuron_13 >> 3) + (neuron_24 >> 2) + (neuron_4 >> 2);
+            neuron_14 <= (neuron_14 >> 3) + (neuron_1 >> 2) + (neuron_21 >> 1);
+            neuron_15 <= (neuron_15 >> 3) + (neuron_28 >> 2) + (neuron_19 >> 1);
+            neuron_16 <= (neuron_16 >> 3) + (neuron_8 >> 1) + (neuron_26 >> 1);
+            neuron_17 <= (neuron_17 >> 3) + (neuron_1 >> 1) + (neuron_8 >> 2);
+            neuron_18 <= (neuron_18 >> 3) + (neuron_17 >> 1) + (neuron_27 >> 1);
+            neuron_19 <= (neuron_19 >> 3) + (neuron_2 >> 2) + (neuron_13 << 1);
+            neuron_20 <= (neuron_20 >> 3) + (neuron_0 >> 1) + (neuron_9 >> 2);
+            neuron_21 <= (neuron_21 >> 3) + (neuron_11 << 1) + (neuron_21 >> 2);
+            neuron_22 <= (neuron_22 >> 3) + (neuron_26 << 1) + (neuron_8 << 1);
+            neuron_23 <= (neuron_23 >> 3) + (neuron_12 << 1) + (neuron_21 << 1);
+            neuron_24 <= (neuron_24 >> 3) + (neuron_8 >> 2) + (neuron_25 >> 1);
+            neuron_25 <= (neuron_25 >> 3) + (neuron_4 >> 1) + (neuron_28 >> 1);
+            neuron_26 <= (neuron_26 >> 3) + (neuron_7 >> 2) + (neuron_28 << 1);
+            neuron_27 <= (neuron_27 >> 3) + (neuron_7 << 1) + (neuron_7 >> 2);
+            neuron_28 <= (neuron_28 >> 3) + (neuron_11 << 1) + (neuron_16 >> 1);
+            neuron_29 <= (neuron_29 >> 3) + (neuron_7 >> 2) + (neuron_14 << 1);
         end
     end
 

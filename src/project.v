4c8d4779e942365a6a8e71853d7671034a6cee00
mauro-ciccone <269277793+mauro-ciccone@users.noreply.github.com>
2026-09-17 19:31:45 +0200

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_voice_calculator_mauro_ciccone (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
